82f1c3333ee7d447800093cbc0b0bd7af6c4ec0c
EduardaB <eduardacbianquine@gmail.com>
2020-05-07 22:00:46 -0300

    Leitura dos dados

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    double rs, rps, rgc, gm, ge;
+    cout << "Renda anual com salário: ";
+    cin >> rs;
+    cout << "Renda anual com prestação de serviço: ";
+    cin >> rps;
+    cout << "Renda anual com ganho de capital: ";
+    cin >> rgc;
+    cout << "Gastos médicos: ";
+    cin >> gm;
+    cout << "Gastos educacionais: ";
+    cin >> ge;
+
     return 0;
 }
